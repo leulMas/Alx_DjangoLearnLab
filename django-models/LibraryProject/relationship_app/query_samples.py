53e75b9b4915c534b346ac4355ed2d258b687e6a
leul Masresha <leulmasresha19@gmail.com>
2025-07-20 11:24:50 +0300

    complex ORM model

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -5,23 +5,23 @@ import django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_models.settings')
 django.setup()
 
-from relationship_app.models import Author, Book, Library
+from relationship_app.models import Author, Book, Library, Librarian
 
 def run_queries():
-    
+ 
     author_name = "Jane Austen"
     author = Author.objects.get(name=author_name)
     books_by_author = Book.objects.filter(author=author)  # Required line
     print(f"Books by {author_name}: {[book.title for book in books_by_author]}")
 
-    # List all books in a library
+  
     library_name = "Central Library"
     library = Library.objects.get(name=library_name)
     books_in_library = library.books.all()
     print(f"Books in {library_name}: {[book.title for book in books_in_library]}")
 
-    # Retrieve the librarian for a library
-    librarian = library.librarian
+   
+    librarian = Librarian.objects.get(library=library)  # Required line
     print(f"Librarian of {library_name}: {librarian.name}")
 
 if __name__ == "__main__":
