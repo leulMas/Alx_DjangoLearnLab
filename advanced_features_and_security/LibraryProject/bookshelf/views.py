3543b0926e774fdfcc305d160e40e0f49ec83ae8
leul Masresha <leulmasresha19@gmail.com>
2025-07-31 16:07:38 +0300

    second

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,4 +1,42 @@
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import permission_required
+from .models import Book
+from .forms import BookForm  # assume this form exists
 
 def home(request):
     return HttpResponse("Hello! Welcome to the custom user model app.")
+
+
+@permission_required('advanced_features_and_security.can_view', raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'book_list.html', {'books': books})
+
+@permission_required('advanced_features_and_security.can_create', raise_exception=True)
+def add_book(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm()
+    return render(request, 'book_form.html', {'form': form})
+
+@permission_required('advanced_features_and_security.can_edit', raise_exception=True)
+def edit_book(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    form = BookForm(request.POST or None, instance=book)
+    if form.is_valid():
+        form.save()
+        return redirect('book_list')
+    return render(request, 'book_form.html', {'form': form})
+
+@permission_required('advanced_features_and_security.can_delete', raise_exception=True)
+def delete_book(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == 'POST':
+        book.delete()
+        return redirect('book_list')
+    return render(request, 'confirm_delete.html', {'book': book})
