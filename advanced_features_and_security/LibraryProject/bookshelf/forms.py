ef8e4058b362ed9d9c5c973d4eb2daff970345c2
leul Masresha <leulmasresha19@gmail.com>
2025-07-31 16:20:54 +0300

    second

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,3 +1,11 @@
+from django import forms
+from .models import Book
+
+class ExampleForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ['title', 'author']
+
 <form method="post">
     {% csrf_token %}
     {{ form.as_p }}
