376f2154368b99185893efe2ef328dd52d03a712
leul Masresha <leulmasresha19@gmail.com>
2025-08-10 13:13:49 -0400

    api2

--- advanced-api-project/advanced_api_project/urls.py
+++ advanced-api-project/advanced_api_project/urls.py
@@ -4,14 +4,7 @@ from django.urls import path, include
 from django.shortcuts import redirect
 from rest_framework import routers
 from api.views import AuthorViewSet, BookViewSet
-from django.urls import path
-from .views import (
-    BookListView,
-    BookDetailView,
-    BookCreateView,
-    BookUpdateView,
-    BookDeleteView,
-)
+
 router = routers.DefaultRouter()
 router.register(r'authors', AuthorViewSet)
 router.register(r'books', BookViewSet)
@@ -20,9 +13,6 @@ urlpatterns = [
     path('', lambda request: redirect('/api/')),  # root → /api/
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
-    path('books/', BookListView.as_view(), name='book-list'),
-    path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
-    path('books/create/', BookCreateView.as_view(), name='book-create'),
-    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),
-    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),
+    path('api/', include('api.urls')),
+
 ]
--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -8,9 +8,9 @@ from .views import (
 )
 
 urlpatterns = [
-    path('books/', BookListView.as_view(), name='book-list'),                  # List all books
-    path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),     # Retrieve book by ID
-    path('books/create/', BookCreateView.as_view(), name='book-create'),       # Create new book
-    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),  # Update existing book
-    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),  # Delete a book
+    path('books/', BookListView.as_view(), name='book-list'),                # List all books
+    path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),   # Retrieve one book by ID
+    path('books/create/', BookCreateView.as_view(), name='book-create'),     # Create a new book
+    path('books/update/<int:pk>/', BookUpdateView.as_view(), name='book-update'),  # Update a book
+    path('books/delete/<int:pk>/', BookDeleteView.as_view(), name='book-delete'),  # Delete a book
 ]
