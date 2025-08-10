2fe7775c474aee69fa62968158469249a4f0378d
leul Masresha <leulmasresha19@gmail.com>
2025-08-10 13:45:48 -0400

    api2

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,12 +1,12 @@
 from rest_framework import filters
 from django_filters import rest_framework
-from rest_framework import generics, permissions, filters
+from rest_framework import generics,filters
 from .models import Book
 from .serializers import BookSerializer
-from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework import generics
 from rest_framework.filters import OrderingFilter, SearchFilter
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 
 class BookListView(generics.ListAPIView):
