9b7b47061bd7156d3f559887d0cc482eba1bec96
leul Masresha <leulmasresha19@gmail.com>
2025-08-10 13:29:36 -0400

    api2

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -5,6 +5,7 @@ from .serializers import BookSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.filters import OrderingFilter, SearchFilter
 from rest_framework import generics
 
 class BookListView(generics.ListAPIView):
