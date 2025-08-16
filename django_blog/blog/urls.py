cd048fbc794234e132d6cf48360c248219142691
leul Masresha <leulmasresha19@gmail.com>
2025-08-16 14:02:05 -0400

    auth

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path("login/",  BlogLoginView.as_view(),  name="login"),
     path("logout/", BlogLogoutView.as_view(), name="logout"),
     path("register/", register, name="register"),
-    path("profile/",  profile,  name="profile"),
+    path("profile/", profile, name="profile"),
 ]
