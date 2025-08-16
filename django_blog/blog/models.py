46fed3037ede5f2d7501402c3b4ca3b12b249e76
leul Masresha <leulmasresha19@gmail.com>
2025-08-16 13:58:27 -0400

    auth

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,14 +1,11 @@
-from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from django.db import models
+from django.contrib.auth.models import User
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("blog.urls")),
-]
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
 
-# Serve static/media in development
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    def __str__(self):
+        return self.title
