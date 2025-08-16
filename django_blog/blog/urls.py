70226944e8d74bc742b5fa35f2e6321403d62fd5
leul Masresha <leulmasresha19@gmail.com>
2025-08-16 15:11:22 -0400

    managment_features

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -6,7 +6,7 @@ from .views import (
 )
 
 urlpatterns = [
-    # Existing views
+    # Existing function-based views
     path("", views.home_view, name="home"),
     path("register/", views.register_view, name="register"),
     path("login/", views.login_view, name="login"),
