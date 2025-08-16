20458f0eb26bfee8f88a2f3d6b22940819b32c38
leul Masresha <leulmasresha19@gmail.com>
2025-08-16 14:52:58 -0400

    auth

--- django_blog/django_blog/urls.py
+++ django_blog/django_blog/urls.py
@@ -1,8 +1,7 @@
-# django_blog/urls.py
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("blog.urls")),
+    path('admin/', admin.site.urls),
+    path('', include('blog.urls')),  # Include your blog app URLs
 ]
