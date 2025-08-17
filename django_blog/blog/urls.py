b44ed4a420277ee273bab1fb3b45a587ad480e37
leul Masresha <leulmasresha19@gmail.com>
2025-08-17 11:39:34 -0400

    new

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 from .views import PostListView, PostDetailView,PostCreateView, PostUpdateView, PostDeleteView
 
+
 urlpatterns = [
     # Existing function-based views
     path("", views.home_view, name="home"),
@@ -17,4 +18,5 @@ urlpatterns = [
     path['posts/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'],  # Update a post
     path['posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'], # Delete a post
     path["post/<int:pk>/delete/", "post/<int:pk>/update/", "post/new/"]
+    path["comment/<int:pk>/update/", "post/<int:pk>/comments/new/", "comment/<int:pk>/delete/"]
 ]
