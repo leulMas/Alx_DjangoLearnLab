62177558e8d54f05e72e625e0c50b561f6d3edb9
leul Masresha <leulmasresha19@gmail.com>
2025-08-17 11:44:04 -0400

    new

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -3,6 +3,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Post
+from taggit.forms import TagWidget
 
 
 class CustomUserCreationForm(UserCreationForm):
@@ -29,3 +30,16 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['content']
+
+class ExampleForm(forms.ModelForm):
+    class Meta:
+        model = Example
+        fields = ('name', 'tags',)
+        widgets = {
+            'tags': TagWidget(attrs={
+                'class': 'form-control',
+                'id': 'tags',
+                'placeholder': 'Enter tags separated by commas',
+                'data-role': 'tagsinput'
+            })
+        }
