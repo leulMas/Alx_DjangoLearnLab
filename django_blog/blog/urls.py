20067746a9b24b242457686242193ea80b32a58b
leul Masresha <leulmasresha19@gmail.com>
2025-08-17 11:28:02 -0400

    new

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -11,9 +11,9 @@ urlpatterns = [
     path("profile/", views.profile_view, name="profile"),
 
     # Blog post CRUD views
-    path('posts/', PostListView.as_view(), name='post-list'),               # List all posts
-    path('posts/new/', PostCreateView.as_view(), name='post-create'),       # Create a new post
-    path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),  # View a single post
-    path('posts/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),  # Update a post
-    path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'), # Delete a post
+    path['posts/', PostListView.as_view(), name='post-list'],               # List all posts
+    path['posts/new/', PostCreateView.as_view(), name='post-create'],       # Create a new post
+    path['posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'],  # View a single post
+    path['posts/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'],  # Update a post
+    path['posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'], # Delete a post
 ]
