1afe2b6d1040a0d2e40fbafa2648c7de92cf84ad
leul Masresha <leulmasresha19@gmail.com>
2025-08-30 13:04:06 -0400

    posts

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -13,5 +13,7 @@ router.register(r"feed", FeedViewSet, basename="feed""
 
 urlpatterns = [
     path("", include(router.urls)),
+    path("follow/<int:user_id>/", views.follow_user, name="follow-user"),
+    path("unfollow/<int:user_id>/", views.unfollow_user, name="unfollow-user"),
 ]
 
