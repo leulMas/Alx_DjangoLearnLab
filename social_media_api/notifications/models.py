d1c44cad0de0c7c965effc92bcc23b667fba93cf
leul Masresha <leulmasresha19@gmail.com>
2025-08-30 13:37:43 -0400

    posts

--- social_media_api/notifications/models.py
+++ social_media_api/notifications/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.conf import settings
 
-# Create your models here.
+User = settings.AUTH_USER_MODEL
+
+class Notification(models.Model):
+    recipient = models.ForeignKey(User, related_name="notifications", on_delete=models.CASCADE)
+    actor = models.ForeignKey(User, related_name="actions", on_delete=models.CASCADE)
+    verb = models.CharField(max_length=255)  # e.g., "liked your post"
+    
+    # Generic relation to target object (like Post, Comment, etc.)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    target = GenericForeignKey('content_type', 'object_id')
+    
+    read = models.BooleanField(default=False)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.actor} {self.verb} to {self.recipient}"
